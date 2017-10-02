412b9de0b34ececbb715c47a055a63b541127a31
cyyongoueth <31543342+cyyongoueth@users.noreply.github.com>
2017-10-01 19:45:21 -0500

    Add files via upload

---
+++ homework/2/script_full_of_errors_fixed.m
@@ -0,0 +1,26 @@
+a = 2; %You cannot have a number before a letter
+a1 = 2;
+x = 2;
+y = x + 4; % is it 6? %Changed the variable to lowercase x so that it matches above
+pi =4*atan(1);
+disp(pi)
+pi = 3.14159; %This should not have quoation marks
+disp(tan(pi));
+c = 4^3^2^3;
+tt = ((c-78564)/c + 32); %Took off an extra parantheses 
+year = '2017';
+disp(['The year is ' year]); % does it display 2017 correctly? No. So I created a year variable 
+stuff = {'a' 'b' 4 21 'c'};
+beginning = stuff(1); %Changed to 1 instead of 0 because 1 is a, where it starts
+enddd = stuff(5);
+discount = 12; %Added semi colon
+AMOUNT = 120; % Deleted the line & Added a semi colon
+amount = '120$'; %Added quotation marks
+and = 'duck'; %Changed to statement line because duck is not defined 
+class = 'INF1100, gr 2'; %Changed the double quotation marks to a single
+continue_ = x > 0;
+fox = false;
+wolf = fox == true;
+Persian = ['Persian' ' is ' 'a' ' human ' 'language']; % Removed semi-colon and comma
+Spanish = {'Spanish ' 'is ' ' another'  'language'};
+disp(['Persian is not the same as Spanish']); %Persian needs to be in the quotation marks because it is already defined as another sentence
